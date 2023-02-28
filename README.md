0beda1b6c5954e48b7b9ce78904753cb8591b1da
eyxnz <todaytubuck@naver.com>
2023-02-28 13:07:06 +0900

    #1 - 스프링 부트 프로젝트 초기화
    들어간 초기 기술은
    * Spring Web
    * Lombok
    * Spring Boot Devtools

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fastcampus-loan
+
 패스트 캠퍼스 대출 프로젝트
---
+++ src/main/java/com/fastcampus/loan/FastcampusLoanApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusLoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusLoanApplication.class, args);
+    }
+
+}
