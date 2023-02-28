36e762f20227098bde5cd536395a2cccdfa4e484
eyxnz <todaytubuck@naver.com>
2023-03-01 00:52:27 +0900

    #5 - 대출 상담 조회 테스트 구현

--- src/test/java/com/fastcampus/loan/service/CounselServiceTest.java
+++ src/test/java/com/fastcampus/loan/service/CounselServiceTest.java
@@ -2,7 +2,10 @@ package com.fastcampus.loan.service;
 
 import com.fastcampus.loan.domain.Counsel;
 import com.fastcampus.loan.dto.CounselDTO;
+import com.fastcampus.loan.exception.BaseException;
+import com.fastcampus.loan.exception.ResultType;
 import com.fastcampus.loan.repository.CounselRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -12,6 +15,9 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,6 +28,7 @@ class CounselServiceTest {
     @Mock private CounselRepository counselRepository;
     @Spy private ModelMapper modelMapper;
 
+    @DisplayName("새로운 상담 Entity Request 를 입력으로 넣으면 해당 상담 Entity 의 Response 를 반환한다.")
     @Test
     void givenRequestOfNewCounselEntity_whenRequestCounsel_thenReturnResponseOfNewCounselEntity() {
         // Given
@@ -54,5 +61,39 @@ class CounselServiceTest {
         // Then
         assertThat(actual.getName())
                 .isSameAs(entity.getName());
+    }
+
+    @DisplayName("존재하는 상담 Id 를 입력으로 넣으면 해당 상담 Entity 의 Response 를 반환한다.")
+    @Test
+    void givenCounselId_whenRequestExistCounselId_thenReturnResponseOfExistCounselEntity() {
+        // Given
+        Long findId = 1L;
+
+        Counsel entity = Counsel.builder()
+                .counselId(1L)
+                .build();
+
+        when(counselRepository.findById(findId))
+                .thenReturn(Optional.ofNullable(entity));
+
+        // When
+        CounselDTO.Response actual = counselService.get(findId);
+
+        // Then
+        assertThat(actual.getCounselId())
+                .isSameAs(findId);
+    }
+
+    @DisplayName("존재하지 않는 상담 Id 를 입력으로 넣으면 예외를 발생시킨다.")
+    @Test
+    void givenCounselId_whenRequestNotExistCounselId_thenThrowException() {
+        // Given
+        Long findId = 2L;
+
+        when(counselRepository.findById(findId))
+                .thenThrow(new BaseException(ResultType.SYSTEM_ERROR));
+
+        // When & Then
+        assertThrows(BaseException.class, () -> counselService.get(findId));
     }
 }
