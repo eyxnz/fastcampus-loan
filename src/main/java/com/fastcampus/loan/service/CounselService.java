fdc2aea9ba1d572cb490a298877630a574179498
eyxnz <todaytubuck@naver.com>
2023-03-01 00:31:33 +0900

    #5 - 대출 상담 등록 서비스 구현

---
+++ src/main/java/com/fastcampus/loan/service/CounselService.java
@@ -0,0 +1,27 @@
+package com.fastcampus.loan.service;
+
+import com.fastcampus.loan.domain.Counsel;
+import com.fastcampus.loan.dto.CounselDTO;
+import com.fastcampus.loan.repository.CounselRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class CounselService {
+    private final CounselRepository counselRepository;
+    private final ModelMapper modelMapper;
+
+    // 대출 상담 등록
+    public CounselDTO.Response create(CounselDTO.Request request) {
+        Counsel counsel = modelMapper.map(request, Counsel.class);
+        counsel.setAppliedAt(LocalDateTime.now());
+
+        Counsel created = counselRepository.save(counsel);
+
+        return modelMapper.map(created, CounselDTO.Response.class);
+    }
+}
