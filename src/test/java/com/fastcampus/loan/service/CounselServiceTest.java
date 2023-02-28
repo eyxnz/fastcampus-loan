2b80e6bed6474e9ec3d65f363f9243b31904fd0e
eyxnz <todaytubuck@naver.com>
2023-03-01 00:25:29 +0900

    #5 - 대출 상담 등록 테스트 구현

---
+++ src/test/java/com/fastcampus/loan/service/CounselServiceTest.java
@@ -0,0 +1,58 @@
+package com.fastcampus.loan.service;
+
+import com.fastcampus.loan.domain.Counsel;
+import com.fastcampus.loan.dto.CounselDTO;
+import com.fastcampus.loan.repository.CounselRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class CounselServiceTest {
+    @InjectMocks CounselService counselService;
+
+    @Mock private CounselRepository counselRepository;
+    @Spy private ModelMapper modelMapper;
+
+    @Test
+    void givenRequestOfNewCounselEntity_whenRequestCounsel_thenReturnResponseOfNewCounselEntity() {
+        // Given
+        Counsel entity = Counsel.builder()
+                .name("Member Kim")
+                .cellPhone("010-1111-2222")
+                .email("mail@abc.de")
+                .memo("I hope to get a loan")
+                .zipCode("123456")
+                .address("Somewhere in Gangnam-gu, Seoul")
+                .addressDetail("What Apartment No. 101, 1st floor No. 101")
+                .build();
+
+        CounselDTO.Request request = CounselDTO.Request.builder()
+                .name("Member Kim")
+                .cellPhone("010-1111-2222")
+                .email("mail@abc.de")
+                .memo("I hope to get a loan")
+                .zipCode("123456")
+                .address("Somewhere in Gangnam-gu, Seoul")
+                .addressDetail("What Apartment No. 101, 1st floor No. 101")
+                .build();
+
+        when(counselRepository.save(ArgumentMatchers.any(Counsel.class)))
+                .thenReturn(entity);
+
+        // When
+        CounselDTO.Response actual = counselService.create(request);
+
+        // Then
+        assertThat(actual.getName())
+                .isSameAs(entity.getName());
+    }
+}
